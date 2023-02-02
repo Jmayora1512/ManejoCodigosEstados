d9450d302351d1e56bab062144e2edfded4f04bc
Jmayora1512 <jorgemayora88@gmail.com>
2023-02-02 19:04:55 +0100

    Se agrega el proyecto

---
+++ src/web/Servlet.java
@@ -0,0 +1,66 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+        //Simulando los valores correctos
+        String usuarioOk = "Juan";
+        String passwordOk = "123";
+        
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        PrintWriter out = response.getWriter();
+        
+        if(usuarioOk.equals(usuario) && passwordOk.equals(password)){
+            out.print("<h1>");
+            out.print("Datos correctos");
+            out.print("<br>Usuario:" + usuario);
+            out.print("<br>Password:" + password);
+            out.print("</h1>");
+        }
+        else{
+            response.sendError(response.SC_UNAUTHORIZED, "Las credenciales son incorrectas");
+        }
+        out.close();		
+		
+	}
+
+}
